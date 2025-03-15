fc5531de98f1318c6928e534365643ea60790222
Ravi Rao <nag.raavi@gmail.com>
2025-03-15 21:47:39 +0000

    Added GitHub Actions CI workflow

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  moduleNameMapper: {
-    "^react-router-dom$": "<rootDir>/node_modules/react-router-dom"
-  },
-  testEnvironment: "jest-environment-jsdom",
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
-  },
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"]
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  testEnvironment: "jsdom", // Ensures a browser-like environment
 };
