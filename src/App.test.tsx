7f91a670d05989b27a3b4023a28e7ca6304d4f66
Ravi Rao <nag.raavi@gmail.com>
2025-03-15 18:31:06 +0000

    Added GitHub Actions CI workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
